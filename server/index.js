063158886d410549c9222a6786d5ea5dea00a68c
NadunAtGit <nadunsoor404@gmail.com>
2025-01-30 04:03:11 +0530

    client build

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(express.static("../client/dist"));
 
 app.get("*",(req,res)=>{
   res.sendFile(path.resolve(__dirname,"..","client","dist","index.html"))
